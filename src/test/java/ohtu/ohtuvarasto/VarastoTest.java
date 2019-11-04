8eae69c6d3a50088a76141c39db2e2542bb022e8
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-11-04 22:33:24 +0200

    tehtävä 10 testi korjattu

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -87,7 +87,7 @@ public class VarastoTest {
     @Test
     public void tilavuudenYlittysEiMuutaSaldoa() {
         varasto.lisaaVarastoon(100);
-        assertEquals(100, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
     }
 
     @Test
