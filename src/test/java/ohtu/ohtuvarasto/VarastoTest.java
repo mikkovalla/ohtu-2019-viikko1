a78cff882c3d7b328a680f6542d0bceeb0cfcfe7
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2019-11-04 22:25:55 +0200

    tehtävä 8

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -8,6 +8,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class VarastoTest {
@@ -63,6 +64,57 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uusiVarastoLuodaanNollaTilavuudellaJosTilavuusAlleNolla() {
+        varasto = new Varasto(-10);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uusiVarastoLuodaanNollaTilavuudellaJosTilavuusAlleNollaJaAlkusaldoAnnettu() {
+        varasto = new Varasto(-10, 20);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uusiVarastoLuotoAlkuSaldolla() {
+        varasto = new Varasto(20, 10);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void tilavuudenYlittysEiMuutaSaldoa() {
+        varasto.lisaaVarastoon(100);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivinenLisaysEiMuutaArvoja() {
+        varasto.lisaaVarastoon(-10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }  
+    
+    @Test
+    public void saldoaSuurempiOttoEiVieSaldoaNegatiiviseksi() {
+        varasto.lisaaVarastoon(5);
+        varasto.otaVarastosta(10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    } 
+
+    @Test
+    public void negatiivisellaArvollaOttaminenEiTeeMitaan() {
+        varasto.lisaaVarastoon(10);
+        double otettu = varasto.otaVarastosta(-20);
+        assertEquals(0, otettu, vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivisellaArvollaOttaminenEiMuutaSaldoa() {
+        varasto.lisaaVarastoon(10);
+        double otettu = varasto.otaVarastosta(-20);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
     }
 
 }
